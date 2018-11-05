e5c0bb898a4c9642807cf7ac7cde5c0643cdd395
yqyan2010 <yqyan2010@gmail.com>
2018-11-04 21:57:39 -0800

    2nd commit

--- femonicr_plotwclass.py
+++ femonicr_plotwclass.py
@@ -14,27 +14,46 @@ class Element:
         #self.linuxpath='/home/purity/Desktop/Data'
         self.winpath='C:\\Users\\yyan\\Documents\\Data\\ICPMS\\Alldata'
         self.filepath=join(self.winpath,self.filename)
-        self.df_sample=pd.read_excel(self.filepath,sheetname="sample",parse_cols="A:H")
+        #Loading all sample info (not needed for now)
+        #self.df_sample=pd.read_excel(self.filepath,sheetname="sample",parse_cols="A:H")
+        #Load all elements data
         self.df_elements=pd.read_excel(self.filepath,sheetname="sample",parse_cols="J:AM")
+        #Get only one element data
+        self.data=self.df_elements[self.element]
 
     def scatter(self):
-        data=self.df_elements[self.element]
-        num_data=self.df_elements.index.values
-        #fig=plt.figure()
-        #plot=fig.add_subplot(111)
-        #plot.scatter(num_data,data)
-        return plt.scatter(num_data,data)
+        #data=self.df_elements[self.element]
+        num_data=self.data.index.values
+        return plt.scatter(num_data,self.data)
     
     def histogram(self,binval=None):
-        data=self.df_elements[self.element]
+        #data=self.df_elements[self.element]
         binval = binval
-        return plt.hist(data,bins=binval)
+        return plt.hist(self.data,bins=binval)
     
     def mean(self):
-        return self.df_elements[self.element].mean()
+        return self.data.mean()
+    
+    def quantile(self,val=None):
+        val=val or 0.5
+        return self.data.quantile(val)
+    
+    def convert(self):
+        return self.data
+        
+    def zero(self):
+        return (self.data==0).any()
+    
+    def total(self):
+        return len(self.data)
+    
+    def numzero(self):
+        nozerodata=self.data[self.data>0]
+        return len(self.data)-len(nozerodata)
+        
 """Plot"""
 
-"""Output"""
+"""Output
 
 """End of script"""
 print("end of script")
